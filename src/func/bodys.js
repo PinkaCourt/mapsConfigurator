28ffd5d94426968d975a6448c848cfab9789a575
PinkaCourt <ekortika@gmail.com>
2020-05-09 17:50:50 +0300

    разнесение на модули

--- src/func/bodys.js
+++ src/func/bodys.js
@@ -1,42 +1,38 @@
-export class MapStr {
+import {getUuid} from './getUuid.js'
+
+export class MapProps {
     constructor() {
-      this.name = 'NBL_Postman_Map99';
+      this.name = new Date;
       this.type = 'MAP_TYPE_GEO';
       this.position = {
         x: localStorage.getItem('longitude'),
         y: localStorage.getItem('latitude')
       };
       this.zoom = 17;
-      this.provider_id = providerID
+      this.provider_id = '9cb89d76-67e9-47cf-8137-b9ee9fc46388'
     }
   }
 
-
-
-
-  export class MapObj {
+  export class Map {
     constructor() {
       this.id = getUuid();
       this.sharing = {
         kind: 'SHARING_KIND_NOT_SHARED',
         shared_roles: []
       };
-      this.map = new MapStr;
+      this.map = new MapProps;
     }
   }
-
-
   
 export class ToCreate {
     constructor() {
-      this.created = new MapObj;
+      this.created = new Map;
     }
   }
 
-
  export  class ToDelete {
-    constructor() {
-      this.removed = [toDeleteMaps];
+    constructor(array) {
+      this.removed = [array];
     }
   }
   
--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import makeElement from './func/makeElement.js'
 import {GetOptions} from "./func/httpapi.js"
 import {PostOptions} from "./func/httpapi.js"
 
+import {ToCreate} from "./func/bodys.js"
+import {ToDelete} from "./func/bodys.js"
+//import {MapStr} from "./func/bodys.js"
+//import {Map} from "./func/bodys.js"
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -21,16 +26,23 @@ const AUTH = 'Basic ' + btoa(USER + ':' + PASS);
 
 const URL = 'http://' + IP + ':' + PORT
 const URLhosts = URL + '/hosts'
-//const URLIndexByRole = URL + '/v1/security/config/user'
-const URLcreateMap =   URL + '/v1/maps:change'
+const URLchangeMap=   URL + '/v1/maps:change'
 const URLmaplist =   URL + '/v1/maps?view=VIEW_MODE_ONLY_META'
 
 
-const providerID = '9cb89d76-67e9-47cf-8137-b9ee9fc46388';
+//const providerID = '9cb89d76-67e9-47cf-8137-b9ee9fc46388';
 const getHostButton = document.getElementById('getHost')
 const addingMarkersButton = document.getElementById('addingMarkersButton')
 const removeMapsButton = document.getElementById('removeMapsButton')
 const resultDiv = document.getElementById('resultDiv')
+
+let toDeleteMaps = [];
+
+function selectID(e) {
+  let value = e.meta.id;
+  toDeleteMaps.push(JSON.stringify(value));
+}
+
 
 getHostButton.addEventListener("click", async function() {
   let host = await fetch(URLhosts, new GetOptions(AUTH))
@@ -42,16 +54,16 @@ getHostButton.addEventListener("click", async function() {
       makeElement('resultDiv', data[0])
     });
 });
-
+/*
 function  getUuid() {
   return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
     (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
   )
 }
-
+*/
 
 createMapButton.addEventListener("click", async function() {
-  map = await fetch(URLcreateMap, new PostOptions(AUTH, new ToCreate))
+  let map = await fetch(URLchangeMap, new PostOptions(AUTH, new ToCreate))
     .then(res =>
       fetch(URLmaplist, new GetOptions(AUTH))
       .then(res => res.json())
@@ -61,24 +73,21 @@ createMapButton.addEventListener("click", async function() {
 
 
 addingMarkersButton.addEventListener("click", async function() {
-  mapList = await fetch(URLmaplist, new GetOptions(AUTH))
+  let mapList = await fetch(URLmaplist, new GetOptions(AUTH))
     .then(res => res.json())
     .then(data =>
-      data.items[0].meta.id)
+      data.items.map(selectID))
   makeElement('resultDiv', mapList);
 });
 
 var geoPosition
 
 document.getElementById('getGeolocation').addEventListener("click", function() {
-  //(navigator.geolocation)?console.log('DONE'):console.log('fail');
   function success (position) {
-    const longitude = position.coords.longitude;
-    const latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    let latitude = position.coords.latitude;
     localStorage.setItem('longitude', position.coords.longitude);
     localStorage.setItem('latitude', position.coords.latitude);
-    makeElement('resultDiv', longitude + '  ')
-    makeElement('resultDiv', latitude)
   }
 
   function error (error) {
@@ -88,17 +97,15 @@ document.getElementById('getGeolocation').addEventListener("click", function() {
   geoPosition = navigator.geolocation.getCurrentPosition(success,  error  )
 });
 
-let toDeleteMaps = [];
-
-function selectID(e) {
-  value = e.meta.id;
-  toDeleteMaps.push(JSON.stringify(value));
-}
-
 removeMapsButton.addEventListener("click", async function() {
   let mapList = await fetch(URLmaplist, new GetOptions(AUTH))
     .then(res => res.json())
-    .then(data => data.items[0].meta.id)     
+    .then(data => 
+      array = data.items.map(selectID))
+    .then(() =>
+        fetch(URLchangeMap, new ToDelete(array))
+        .then(res => res.json())
+        )     
 });
 
 document.getElementById('barrelButton').addEventListener("click", function() {
