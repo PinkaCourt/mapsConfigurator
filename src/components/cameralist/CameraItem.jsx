a22806dff0414c633cc2b728352d5e3d28a87029
PinkaCourt <ekortika@gmail.com>
2020-08-03 01:57:06 +0300

    рефакторинг камеры из списка

--- src/components/cameralist/CameraItem.jsx
+++ src/components/cameralist/CameraItem.jsx
@@ -1,12 +1,8 @@
 import React, {Component} from 'react';
-//здесь тупо по 1 камере/ будет когда-нибудь
 
-const CameraItem = props =>
-  <li className="camera_bm">
-    <span className="Camera_id"> {props.id}</span>
-    <span className="Camera_name"> {props.name}</span>
+const CameraItem = (camera) =>
+  <li className="camera_item">
+    <span> {camera.id} {' '} {camera.name}</span>
   </li>;
 
 export default CameraItem;
-
-//{this.props.name}
