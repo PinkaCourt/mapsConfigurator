0c2244bfd8bbcdbbca248a3ba26e2a5447ffeedc
PinkaCourt <ekortika@gmail.com>
2020-08-07 00:02:30 +0300

    убраны лишние кнопки. переделана форма

--- src/components/topbar/ToolBar.jsx
+++ src/components/topbar/ToolBar.jsx
@@ -11,7 +11,9 @@ import {AUTH} from '../../constants/input.js'
 import {URL} from '../../constants/url.js'
 import {URLhosts} from '../../constants/url.js'
 
-export default class App extends Component {
+
+
+export default class ToolBar extends Component {
 
   async handleClick(AUTH) {
     const res = await fetch(URLhosts, new GetOptions(AUTH))
@@ -24,20 +26,22 @@ export default class App extends Component {
     buttons: [
       {id:'createMap', buttonID: 'createMapButton', value: 'To create Map'},
       {id:'geolocation', buttonID: 'getGeolocation', value: 'Get Geolocation'},
-      {id:'maplist', buttonID: 'maplistButton', value: 'Map List'},
       {id:'removeMaps', buttonID: 'removeMapsButton', value: 'Remove Maps'},
-      {id:'Cameras', buttonID: 'getCamerasbutton', value: 'Get cameras'},
       {id:'AddingCameras', buttonID: 'addCamerasOnMap', value: 'Adding markers on map'},
       {id:'resultDiv', buttonID: 'barrelButton', value: 'do a barrel roll'}
     ],
     forms: [
-      {id:'IP', value: '192.168.1.36'},
+      {id: 'IP', value: '192.168.1.36'},
       {id:'PORT', value: '8001'},
       {id:'USER', value: 'root'},
       {id:'PASS', value: 'root'}
     ],
     hosts:[]
   }
+//ref={newHost}
+
+  //делаем ссылку на новый элемент
+  newHost = React.createRef();
 /*
 пнем хосты ручками
 handle(Click) - это событие реакта. 
@@ -55,26 +59,8 @@ async handleClick(AUTH) {
   render() {
     return (
       <div className="topbar_wrapper"> 
-        <fieldset className="step input" id="input">
-        <legend>Данные сервера</legend>
-        {this.state.forms.map((e, index) => {
-          return (
-            <Form 
-              key = {index}
-              id = {e.id}
-              value = {e.value}  
-              />
-          )
-        })}
-        <button onClick={this.handleClick.bind(this, AUTH)} className="button" id="getHost"> GET HOST </button>
-        <div className="hosts_list">
-          {this.state.hosts.map((e, index) => (
-            <span> {e} </span>
-          ))}
-          </div>
-        </fieldset>
+        <Form />
         <div className="button_list">
-
           {this.state.buttons.map((e, index) => {
             return (
               <Button 
@@ -83,7 +69,6 @@ async handleClick(AUTH) {
                 buttonID = {e.buttonID}
                 value = {e.value}  
                 />
-            
             )
           })}
           </div>
@@ -91,6 +76,4 @@ async handleClick(AUTH) {
       );
   }
 }
-
-//export default App;
 
