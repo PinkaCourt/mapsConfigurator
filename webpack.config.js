79a3e551510fe74ff7053fe89748f546988b608c
PinkaCourt <ekortika@gmail.com>
2020-08-18 00:25:48 +0300

    сборка проекта вместо аксоновского

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,44 @@ const path = require("path");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const {
+	API_HOST = 'http://localhost',
+	API_AUTH = 'root:root',
+	BUILD_DIR = '0', //'dist',
+	BUILD_ROOT = 'D:\\Program Files\\AxxonSoft\\AxxonNext\\public_html', //__dirname,
+} = process.env; // eslint-disable-line no-process-env
+
+const BUILD_PATH = path.resolve(BUILD_ROOT, BUILD_DIR);
+/*
+const SRC_PATH = path.resolve(__dirname, 'src');
+*/
+
+//const AUTH = 'Basic ' + btoa(USER + ':' + PASS);
 module.exports = {
   entry: ['babel-polyfill', './src/index.js'],
   mode: "development",
   output: {
-    filename: "./index.js"
+    filename: "./index.js",
+    path: BUILD_PATH,
   },
   devServer: {
+		proxy: {
+			'/': {
+				target: API_HOST,
+				headers: {
+					Authorization: `Basic ${Buffer.from(API_AUTH).toString('base64')}`, //const AUTH = 'Basic ' + btoa(USER + ':' + PASS);
+				},
+				//ws: true,
+			},
+		},
+	},
+  /*devServer: {
     contentBase: path.join(__dirname, "dist"),
     compress: true,
     port: 9000,
     watchContentBase: true,
     progress: true
-  },
+  },*/
   module: {
     rules: [
       {
@@ -29,13 +54,7 @@ module.exports = {
         use: [
           require.resolve('style-loader'),
           MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader', 
-            options: {
-              importLoaders: 1,
-              //modules: true,
-              //localIdentName: '[name]__[local]--[hash:base64:5]'
-            }}
+          { loader: 'css-loader', options: {importLoaders: 1}},
         ]
       },
       {
