d963e34276e4f07a2f2932c566a446cf799bb1b9
PinkaCourt <ekortika@gmail.com>
2020-08-18 00:50:58 +0300

    убрана форма

--- src/components/topbar/ToolBar.jsx
+++ src/components/topbar/ToolBar.jsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import Button from './buttons/Button.jsx'
-import Form from './Form.jsx'
 import {log} from '../../func/cameras.js'
 import {getHost} from '../../func/cameras.js'
 import {GetOptions} from '../../func/httpapi.js'
@@ -28,12 +27,6 @@ export default class ToolBar extends Component {
       {id:'AddingCameras', buttonID: 'addCamerasOnMap', value: 'Adding markers on map'},
       {id:'resultDiv', buttonID: 'barrelButton', value: 'do a barrel roll'}
     ],
-    forms: [
-      {id: 'IP', value: '192.168.1.36'},
-      {id:'PORT', value: '8001'},
-      {id:'USER', value: 'root'},
-      {id:'PASS', value: 'root'}
-    ],
     hosts:[]
   }
 
@@ -41,8 +34,6 @@ export default class ToolBar extends Component {
 
   render() {
     return (
-      <div className="topbar_wrapper"> 
-        <Form />
         <div className="button_list">
           {this.state.buttons.map((e, index) => {
             return (
@@ -54,7 +45,6 @@ export default class ToolBar extends Component {
                 />
             )
           })}
-          </div>
       </div>
       );
   }
--- src/constants/input.js
+++ src/constants/input.js
@@ -1,8 +1,4 @@
-/*export const IP =  document.getElementById('IP').value
-export const PORT = document.getElementById('PORT').value
-export const USER = document.getElementById('USER').value
-export const PASS = document.getElementById('PASS').value
-*/
+
 // это говно надо будет удалить отсюда нахрен =))
 export const IP =  '192.168.1.36'
 export const PORT = '8001'
