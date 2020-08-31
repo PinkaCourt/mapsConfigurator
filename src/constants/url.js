23b9c55fd2f18cfc630950d36eae8b1be6d789cc
PinkaCourt <ekortika@gmail.com>
2020-08-31 11:07:07 +0300

    проксирование

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,17 +1,11 @@
-import {IP} from '../constants/input.js'
-import {PORT} from '../constants/input.js'
+export const URLgRPC = "/grpc";
+export const URLhosts = "/hosts";
+export const URLcameraList = "/camera/list";
+export const URLchangeMap = "/v1/maps:change";
+export const URLmaplist = "/v1/maps?view=VIEW_MODE_FULL";
+export const URLMapMarkers = "/v1/maps/markers";
+export const URLMapMarkersUpdate = "/v1/maps/markers:update";
 
-//export const URL = 'http://localhost'
+export const URLCameraSnapshot = "/live/media/snapshot/";
 
-export const URL = 'http://' + IP + ':' + PORT
-export const URLgRPC = URL + '/grpc'
-export const URLhosts = URL + '/hosts'
-export const URLcameraList = URL + '/camera/list'
-export const URLchangeMap = URL + '/v1/maps:change'
-export const URLmaplist = URL + '/v1/maps?view=VIEW_MODE_FULL'
-export const URLMapMarkers = URL + '/v1/maps/markers'
-export const URLMapMarkersUpdate = URL + '/v1/maps/markers:update'
-
-export const URLCameraSnapshot = URL + '/live/media/snapshot/'
-
-export const URLCameraInfo = URL + '/camera/list?filter='
+export const URLCameraInfo = "/camera/list?filter=";
